5966c6100d17dcaba693b6948facbf237a2d75c4
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-22 14:15:25 +0200

    Imported register page and added to router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import App from './App.vue'
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
+import Register from './components/Register.vue';
 import BootstrapVue from 'bootstrap-vue';
 
 // Install BootstrapVue
@@ -12,7 +13,8 @@ Vue.use(VueRouter);
 
 const routes = [
 	{path: "/",component: Home},
-	{path: "/login", component: Login}
+	{path: "/login", component: Login},
+	{path: "/register", component: Register}
 ];
 
 const router = new VueRouter({routes: routes, mode: "history"});
