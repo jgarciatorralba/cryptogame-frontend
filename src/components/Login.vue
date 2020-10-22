1bd5d8656bf54546cb5cb618e504a41d22a10aaf
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-22 16:52:30 +0200

    Added reset link in Login view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,9 @@
   <div class="container cont-login">
     <div class="text-center w-100">
       <form class="form-login" autocomplete="off" method="POST" action="">
-        <img class="mb-2" src="./assets/img/cryptos.png" alt="" width="100" height="100">
+        <router-link to="/" class="text-decoration-none text-body">
+          <img class="mb-2" src="./assets/img/cryptos.png" alt="" width="100" height="100">
+        </router-link>
         <h1 class="h3 mb-3 font-weight-normal">
           Please sign in
         </h1>
@@ -14,10 +16,15 @@
           Password
         </label>
         <input type="password" id="loginPassword" name="loginPassword" class="form-control" placeholder="Password" required>
-
         <button class="btn btn-lg btn-primary btn-block" type="submit">
           Sign in
         </button>
+        <p class="my-1">
+          Forgot your password?
+          <router-link to="/reset" class="text-decoration-none text-body">
+            <b>Reset here</b>.
+          </router-link>
+        </p>
         <p class="my-1">
           Don't have an account?
           <router-link to="/register" class="text-decoration-none text-body">
@@ -40,7 +47,7 @@
 
   /* LOGIN PAGE */
   .cont-login {
-    height: 100%;
+    height: 100vh;
     display: -ms-flexbox;
     display: flex;
     -ms-flex-align: center;
