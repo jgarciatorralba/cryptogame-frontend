7ba05d42af7c35d66b513783428dc0c20c9df917
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 13:58:29 +0200

    add boostrap-vue to main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import Home from './components/Home.vue';
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 const routes = [
 	{path: "/",component: Home},
