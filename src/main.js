badb76c84663a66cffdc45d53531f4d03b196960
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 17:57:52 +0200

    added boostrap icons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import BootstrapVue from 'bootstrap-vue'
+import {BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 import Home from './components/Home.vue';
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 const routes = [
 	{path: "/",component: Home},
