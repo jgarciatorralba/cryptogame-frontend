6fe6cc037b4319bf0b1b76f4b28d1cb84c2b316a
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-22 11:38:53 +0200

    Added login path in router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import App from './App.vue'
 import Home from './components/Home.vue';
+import Login from './components/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
 	{path: "/",component: Home},
+	{path: "/login", component: Login}
 ];
 
 const router = new VueRouter({routes: routes, mode: "history"});
