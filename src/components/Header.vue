239b0826449b02b97fde51b3f8209fb532696c3e
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 13:58:44 +0200

    Done header draft

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,2 +1,41 @@
 <template>
+    <div class="container-fluid">
+        <nav class="row">
+            <div class="col-1">
+                <a>LOGO</a>
+            </div>
+            <div class="col-3">
+                <ul>
+                    <li>
+                        <a>Home</a>
+                    </li>
+                    <li>
+                        <a>Ranking</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <form>
+                    <input>
+                    <button class="" type="submit">Search</button>
+                </form>
+            </div>
+            <div class="col-4">
+                <ul>
+                    <li>
+                        <a class="">Profile</a>
+                    </li>
+                    <li>
+                        <a class="">Transactions</a>
+                    </li>
+                    <li>
+                        <a class="">Wallet</a>
+                    </li>
+                    <li>
+                        <a class="">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </template>
