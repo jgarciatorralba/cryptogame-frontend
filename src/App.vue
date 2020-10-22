013c8432ef9d72c1012717dfe13871790833b031
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 14:18:28 +0200

    appheader component importing on the main component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
 	<div>
-		<app-header></app-header>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-import AppHeader from "./components/Header.vue";
-export default {
-	components: {
-		AppHeader
-	}
-};
 </script>
 
 <style lang="scss">
