db519d234709daafd40713bc29dc6497b926622f
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 17:58:29 +0200

    styles on home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,11 +5,13 @@
             <div class="row">
                 <sidebar></sidebar>
                 <div class="col-10">
-                    <table>
+                    <h3 class="text-center">All Cryptocurrencies</h3>
+                    <table class="coins-table mx-auto">
                         <thead>
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
+                                <th>Symbol</th>
                                 <th>Price</th>
                                 <th>24h</th>
                                 <th>7d</th>
@@ -20,6 +22,43 @@
                             <tr>
                                 <td>1</td>
                                 <td>Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                                <td>$239,167,147,880</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                                <td>$239,167,147,880</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                                <td>$239,167,147,880</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                                <td>$239,167,147,880</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Bitcoin</td>
+                                <td>BTC</td>
                                 <td>$12.911,95</td>
                                 <td>+5.04%</td>
                                 <td>+14.2%</td>
@@ -33,6 +72,33 @@
     </div>
 </template>
 
+<style lang="scss" scoped>
+    .coins-table {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        font-family: sans-serif;
+        width: 90%;
+        thead tr {
+            background-color: rgb(33, 150, 243);
+            color: #ffffff;
+            text-align: left;
+        }
+        tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+        tbody tr:nth-of-type(even) {
+            background-color: #f3f3f3;
+        }
+        tbody tr:last-of-type {
+            border-bottom: 2px solid rgb(33, 150, 243);
+        }
+        th, td {
+            padding: 12px 15px;
+        }
+    }
+</style>
+
 <script>
 import AppHeader from "../components/Header.vue";
 import Sidebar from "../components/Sidebar.vue";
@@ -40,7 +106,8 @@ import Sidebar from "../components/Sidebar.vue";
 export default {
     data() {
         return {
-            coins: null
+            coins: null,
+            logged: true
         }
     },
 	components: {
