c36f0f0105d88b229ac717f8c9b8853e616e774a
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 15:23:58 +0200

    created home layout draft

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,56 @@
 <template>
-    <app-header></app-header>
+    <div>
+        <app-header></app-header>
+        <div class="container">
+            <div class="row">
+                <sidebar></sidebar>
+                <div class="col-10">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>24h</th>
+                                <th>7d</th>
+                                <th>Market Cap</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Bitcoin</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                                <td>$239,167,147,880</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import AppHeader from "../components/Header.vue";
 import Sidebar from "../components/Sidebar.vue";
+
 export default {
+    data() {
+        return {
+            coins: null
+        }
+    },
 	components: {
 		Sidebar, AppHeader
-	}
+    },
+    methods: {
+        requestData() {}
+    },
+    mounted() {
+        this.requestData();
+    }
 };
 </script>
