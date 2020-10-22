b238d680d0ef89c25505f00fc69d092781812788
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 17:58:38 +0200

    styles on sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,16 +1,47 @@
 <template>
-	<div class="col-2">
+	<div class="col-2 sidebar">
 		<div>
-			<img class="img-thumbnail" src="https://www.worldfuturecouncil.org/wp-content/uploads/2020/02/dummy-profile-pic-300x300-1.png"/>
+			<img class="img-thumbnail mb-3" src="https://www.worldfuturecouncil.org/wp-content/uploads/2020/02/dummy-profile-pic-300x300-1.png"/>
 		</div>
 		<div>
 			<ul>
-				<li>Username</li>
-				<li>Wallet:</li>
-				<li>Last balance:</li>
-				<li>Most bought:</li>
-				<li>Ranking:</li>
+				<li>
+					<b-icon icon="person-fill"></b-icon>
+					Username
+				</li>
+				<li>
+					<b-icon icon="wallet2"></b-icon>
+					Wallet:
+				</li>
+				<li>
+					<b-icon icon="percent"></b-icon>
+					Last balance:
+				</li>
+				<li>
+					<b-icon icon="bookmark-star"></b-icon>
+					Most bought:
+				</li>
+				<li>
+					<b-icon icon="trophy"></b-icon>
+					Ranking:
+				</li>
 			</ul>
 		</div>
 	</div>
 </template>
+
+<style lang="scss" scoped>
+	.sidebar{
+		border-radius: 6px;
+		box-shadow: 0 0 0 1px #e0e3eb;
+		padding: 20px 10px;
+		ul {
+			padding: 0;
+            list-style-type: none;
+        }
+		li {
+			margin: 0 10px;
+            display: inline-block;
+		}
+	}
+</style>
