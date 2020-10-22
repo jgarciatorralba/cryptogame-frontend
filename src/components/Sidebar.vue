7a6f02b5facff7fee01ffe6cce76fb7111392343
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 15:23:41 +0200

    added sidebar layout draft

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,2 +1,15 @@
 <template>
+	<div class="col-2">
+		<div>
+			<img class="img-thumbnail" src="https://www.worldfuturecouncil.org/wp-content/uploads/2020/02/dummy-profile-pic-300x300-1.png"/>
+		</div>
+		<div>
+			<ul>
+				<li>Username</li>
+				<li>Ranking:</li>
+				<li>Last balance:</li>
+				<li>Most bought:</li>
+			</ul>
+		</div>
+	</div>
 </template>
