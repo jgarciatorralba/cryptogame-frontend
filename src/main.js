52bb5b2252c570bbf40a5daf083209d6ce55e51b
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 18:09:13 +0200

    added new routes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import VueRouter from 'vue-router';
 import {BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 import Home from './components/Home.vue';
-import Ranking from './components/ranking.vue';
+import Ranking from './components/Ranking.vue';
+import Transactions from './components/Transactions.vue';
+import Profile from './components/Profile.vue';
+import Wallet from './components/Wallet.vue';
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
@@ -12,6 +15,9 @@ Vue.use(BootstrapVueIcons);
 const routes = [
 	{path: "/", component: Home},
 	{path: "/ranking", component: Ranking},
+	{path: "/transactions", component: Transactions},
+	{path: "/profile", component: Profile},
+	{path: "/wallet", component: Wallet},
 ];
 
 const router = new VueRouter({routes: routes, mode: "history"});
