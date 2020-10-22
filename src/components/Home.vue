d7fa39c1b8f44f257f6d05b3ca75495810458fec
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 14:18:11 +0200

    imported appheader and sidebar to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,13 @@
 <template>
-
+    <app-header></app-header>
 </template>
+
+<script>
+import AppHeader from "../components/Header.vue";
+import Sidebar from "../components/Sidebar.vue";
+export default {
+	components: {
+		Sidebar, AppHeader
+	}
+};
+</script>
