ff23d190d81cd35664b2cf0c180ab63af74f14ca
guilherme-carra <gui_gc@hotmail.com>
2020-10-22 18:02:40 +0200

    created ranking component

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router';
 import {BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 import Home from './components/Home.vue';
+import Ranking from './components/ranking.vue';
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 
 const routes = [
-	{path: "/",component: Home},
+	{path: "/", component: Home},
+	{path: "/ranking", component: Ranking},
 ];
 
 const router = new VueRouter({routes: routes, mode: "history"});
