f88759179228887ba6db9cded84767ff08790cba
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-23 21:50:15 +0200

    Add chart with actual data from API

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,42 +2,96 @@
   <div class="container">
     <h1 class="text-center my-3">Test</h1>
     <div class="chart-cont border p-3">
-      <div class="chart">Loading data...</div>
-      <p>{{ test }}</p>
+      <div v-if="loading">
+        <p>Loading data...</p>
+      </div>
+      <div v-else>
+        <div class="chart">
+          <canvas id="myChart" width="100%" height="50vh"></canvas>
+        </div>
+      </div>
     </div>
-    <button @click="getData" class="btn btn-primary my-3">
-      Generate chart
-    </button>
   </div>
 </template>
 
 <script>
+import moment from "moment";
+import Chart from "chart.js";
+
 export default {
   data() {
     return {
-      test: "hola",
-      values: [],
+      loading: true,
+      chartData: [],
     };
   },
   methods: {
     getData() {
-      let now = new Date();
-      console.log(now);
+      let curatedData = [];
 
-      // fetch('http://example.com/movies.json')
-      //   .then(response => response.json())
-      //   .then(data => console.log(data));
+      fetch(
+        "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=1"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data.prices)
+
+          data.prices.forEach((el) => {
+            let record = {};
+            record.date = moment(el[0]).format("LT");
+            record.value = el[1];
+            curatedData.push(record);
+          });
+
+          this.chartData = curatedData;
+          this.loading = false;
+        });
     },
-    refreshPageData() {
-      // console.log("Hola!");
-      this.test = "hola updated";
+    refreshChart() {
+      console.log("Hola!");
     },
   },
   mounted() {
     //TODO ajax request first page load
+    this.getData();
 
     // Set Interval to refresh data every 5 min
-    setInterval(this.refreshPageData, 5000);
+    setInterval(this.refreshChart, 5 * 60 * 1000);
+  },
+  updated() {
+    let labels = [];
+    let data = [];
+    this.chartData.forEach((value) => {
+      labels.push(value.date);
+      data.push(value.value);
+    });
+    console.log(labels);
+    console.log(data);
+
+    let ctx = document.getElementById("myChart");
+    let myLineChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "bitcoin",
+            data: data,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false,
+              },
+            },
+          ],
+        },
+      },
+    });
   },
 };
 </script>
