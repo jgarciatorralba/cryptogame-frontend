7233fba36a2f3ac92884bcffd2e6d44aa4277774
guilherme-carra <gui_gc@hotmail.com>
2020-10-23 14:58:46 +0200

    Add content to the transactions page

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -4,8 +4,12 @@
         <div class="container-fluid">
             <div class="row">
                 <sidebar></sidebar>
-                <div class="col-8">
-                    <h3 class="text-center">Transactions</h3>
+                <div class="col-8 text-center">
+                        <h3 class="mb-4">Transactions</h3>
+                    <div class="col-8 mx-auto">
+                        <div class="bg-danger rounded-lg text-light px-5 py-2 mb-1"><span class="font-weight-bold">Someone</span> bought $321,00 in <span class="font-weight-bold">Bitcoins</span> - 44 mins ago</div>
+                        <div class="bg-success rounded-lg text-light px-5 py-2 mb-1"><span class="font-weight-bold">Someone</span> sold $121,00 in <span class="font-weight-bold">Bitcoins</span> - 51 mins ago</div>
+                    </div>
                 </div>
             </div>
         </div>
