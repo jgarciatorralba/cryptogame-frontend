ee6470791a4153abec3375e52db1a4e781f44fd8
guilherme-carra <gui_gc@hotmail.com>
2020-10-23 15:38:57 +0200

    Add wallet table to body

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -5,7 +5,61 @@
             <div class="row">
                 <sidebar></sidebar>
                 <div class="col-8">
-                    <h3 class="text-center">Wallet</h3>
+                    <h3 class="text-center">Wallet <b-icon icon="wallet2"></b-icon></h3>
+                    <table class="coins-table mx-auto">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Symbol</th>
+                                <th>Quantity</th>
+                                <th>24h</th>
+                                <th>7d</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td><img class="coin-icon" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png">Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td><img class="coin-icon" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png">Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td><img class="coin-icon" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png">Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td><img class="coin-icon" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png">Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td><img class="coin-icon" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png">Bitcoin</td>
+                                <td>BTC</td>
+                                <td>$12.911,95</td>
+                                <td>+5.04%</td>
+                                <td>+14.2%</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -13,6 +67,35 @@
 </template>
 
 <style lang="scss" scoped>
+    .coins-table {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        font-family: sans-serif;
+        width: 90%;
+        thead tr {
+            background-color: rgb(33, 150, 243);
+            color: #ffffff;
+            text-align: left;
+        }
+        tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+        tbody tr:nth-of-type(even) {
+            background-color: #f3f3f3;
+        }
+        tbody tr:last-of-type {
+            border-bottom: 2px solid rgb(33, 150, 243);
+        }
+        th, td {
+            padding: 12px 15px;
+        }
+        .coin-icon {
+            width: 32px;
+            height: 32px;
+            margin-right: 10px;
+        }
+    }
 </style>
 
 <script>
