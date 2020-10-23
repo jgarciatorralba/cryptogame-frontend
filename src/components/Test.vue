588a0c9b2a2c98be72502a4731f9d0724465d344
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-23 15:55:55 +0200

    Add test view for crypto details

---
+++ src/components/Test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1 class="text-center my-3">Test</h1>
+    <div class="chart-cont border p-3">
+      <div class="chart">Hola</div>
+    </div>
+    <button @click="getData" class="btn btn-primary my-3">
+      Generate chart
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      values: [],
+    };
+  },
+  methods: {
+    getData() {
+      let now = new Date();
+      console.log(now);
+
+      // fetch('http://example.com/movies.json')
+      //   .then(response => response.json())
+      //   .then(data => console.log(data));
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
