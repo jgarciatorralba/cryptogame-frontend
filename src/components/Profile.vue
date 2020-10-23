eaeae8fc720fbea98f9db4adc6633a08d7e4232f
guilherme-carra <gui_gc@hotmail.com>
2020-10-23 14:33:12 +0200

    Create profile page content

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,11 +2,32 @@
     <div>
         <app-header></app-header>
         <div class="container-fluid">
-            <div class="row-8">
-                <sidebar></sidebar>
-                <div class="col-10">
-                    <h3 class="text-center">Profile</h3>
-                </div>
+            <div class="row">
+                <h3 class="mb-5 mx-auto">User Profile</h3>
+                <form class="col-10 offset-2">
+                        <div class="row">
+                            <div class="col-3 text-right">
+                                <label for="avatar">
+                                    <img src="https://www.worldfuturecouncil.org/wp-content/uploads/2020/02/dummy-profile-pic-300x300-1.png" class="col-6 rounded-circle">
+                                    <input name="avatar" id="avatar" type="file" accept="image/jpeg" class="d-none">
+                                </label>
+                            </div>
+                            <div class="col-6 my-auto">
+                                <p class="h4">Username</p>
+                                <small class="text-muted d-block">Wallet:</small>
+                                <small class="text-muted">Ranking:</small>
+                            </div>
+                            <div class="col-2 offset-3 mt-4">
+                                <label class="d-block">Name</label>
+                                <input type="text">
+                            </div>
+                            <div class="col-6 mt-4">
+                                <label class="d-block">Email</label>
+                                <input type="text">
+                            </div>
+                            <button class="btn-lg btn-primary mx-auto mt-5">Save changes</button>
+                        </div>
+                </form>
             </div>
         </div>
     </div>
@@ -22,8 +43,6 @@ import Sidebar from "../components/partials/Sidebar.vue";
 export default {
     data() {
         return {
-            coins: null,
-            logged: true
         }
     },
 	components: {
