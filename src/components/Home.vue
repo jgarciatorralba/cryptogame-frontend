2cea7bb3511612db2056553d409847fa31992ea9
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-27 00:48:42 +0100

    Add 'v-if' to show sidebar only if logged

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,114 +1,125 @@
 <template>
-    <div>
-        <app-header></app-header>
-        <div class="container-fluid">
-            <div class="row justify-content-center">
-                <sidebar></sidebar>
-                <div class="col-9">
-                    <h3 class="text-center">All Cryptocurrencies</h3>
-                    <table class="coins-table mx-auto">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Symbol</th>
-                                <th>Price</th>
-                                <th>Change (24h)</th>
-                                <th>High (24h)</th>
-                                <th>Low (24h)</th>
-                                <th>Volume</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="coin in coins" v-bind:key="coin.id">
-                                <td>{{coin.market_cap_rank}}</td>
-                                <td>
-                                    <img :src=coin.image class="coin-icon">
-                                    {{ coin.name }}
-                                </td>
-                                <td>{{ coin.symbol }}</td>
-                                <td>${{ new Intl.NumberFormat("de-DE").format(coin.price) }} USD</td>
-                                <td v-if="coin.change >= 0" class="text-success">
-                                    <b-icon icon="caret-up-fill"></b-icon>
-                                    {{ coin.change.toFixed(1) }}%
-                                </td>
-                                <td v-else class="text-danger">
-                                    <b-icon icon="caret-down-fill"></b-icon>
-                                    {{ coin.change.toFixed(1) }}%
-                                </td>
-                                <td>{{ new Intl.NumberFormat("de-DE").format(coin.high) }} US$</td>
-                                <td>{{ new Intl.NumberFormat("de-DE").format(coin.low) }} US$</td>
-                                <td>{{ new Intl.NumberFormat("de-DE").format(coin.volume) }} US$</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  <div>
+    <app-header></app-header>
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <sidebar v-if="user !== null"></sidebar>
+        <div class="col-9">
+          <h3 class="text-center">All Cryptocurrencies</h3>
+          <table class="coins-table mx-auto">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>Change (24h)</th>
+                <th>High (24h)</th>
+                <th>Low (24h)</th>
+                <th>Volume</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="coin in coins" v-bind:key="coin.id">
+                <td>{{ coin.market_cap_rank }}</td>
+                <td>
+                  <img :src="coin.image" class="coin-icon" />
+                  {{ coin.name }}
+                </td>
+                <td>{{ coin.symbol }}</td>
+                <td>
+                  ${{ new Intl.NumberFormat("de-DE").format(coin.price) }} USD
+                </td>
+                <td v-if="coin.change >= 0" class="text-success">
+                  <b-icon icon="caret-up-fill"></b-icon>
+                  {{ coin.change.toFixed(1) }}%
+                </td>
+                <td v-else class="text-danger">
+                  <b-icon icon="caret-down-fill"></b-icon>
+                  {{ coin.change.toFixed(1) }}%
+                </td>
+                <td>
+                  {{ new Intl.NumberFormat("de-DE").format(coin.high) }} US$
+                </td>
+                <td>
+                  {{ new Intl.NumberFormat("de-DE").format(coin.low) }} US$
+                </td>
+                <td>
+                  {{ new Intl.NumberFormat("de-DE").format(coin.volume) }} US$
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    .coins-table {
-        border-collapse: collapse;
-        margin: 25px 0;
-        font-size: 0.9em;
-        font-family: sans-serif;
-        width: 100%;
-        thead tr {
-            background-color: rgb(33, 150, 243);
-            color: #ffffff;
-            text-align: left;
-        }
-        tbody tr {
-            cursor: pointer;
-            border-bottom: 1px solid #dddddd;
-        }
-        tbody tr:nth-of-type(even) {
-            background-color: #f3f3f3;
-        }
-        tbody tr:last-of-type {
-            border-bottom: 2px solid rgb(33, 150, 243);
-        }
-        tbody tr:hover {
-            user-select: none;
-            background-color: #c2c2c281;
-        }
-        th, td {
-            padding: 12px 15px;
-        }
-        .coin-icon {
-            width: 32px;
-            height: 32px;
-            margin-right: 10px;
-        }
-    }
+.coins-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  width: 100%;
+  thead tr {
+    background-color: rgb(33, 150, 243);
+    color: #ffffff;
+    text-align: left;
+  }
+  tbody tr {
+    cursor: pointer;
+    border-bottom: 1px solid #dddddd;
+  }
+  tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+  }
+  tbody tr:last-of-type {
+    border-bottom: 2px solid rgb(33, 150, 243);
+  }
+  tbody tr:hover {
+    user-select: none;
+    background-color: #c2c2c281;
+  }
+  th,
+  td {
+    padding: 12px 15px;
+  }
+  .coin-icon {
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
+  }
+}
 </style>
 
 <script>
 import AppHeader from "../components/partials/Header.vue";
 import Sidebar from "../components/partials/Sidebar.vue";
-import {coinsList} from "../components/mock/coinlist.js";
-import axios from 'axios';
+import { coinsList } from "../components/mock/coinlist.js";
+import axios from "axios";
 
 export default {
-    data() {
-        return {
-            coins: null,
-            logged: true
-        }
+  data() {
+    return {
+      coins: null,
+      user: JSON.parse(localStorage.getItem("user")),
+    };
+  },
+  components: {
+    Sidebar,
+    AppHeader,
+  },
+  methods: {
+    requestData() {
+      axios
+        .get("http://localhost:3000/api/coins")
+        .then((response) => (this.coins = response.data));
     },
-	components: {
-		Sidebar, AppHeader
-    },
-    methods: {
-        requestData() {
-            axios.get('http://localhost:3000/api/coins')
-                .then(response => this.coins = response.data);
-        }
-    },
-    mounted() {
-        this.requestData();
-    }
+  },
+  mounted() {
+    this.requestData();
+  },
 };
 </script>
