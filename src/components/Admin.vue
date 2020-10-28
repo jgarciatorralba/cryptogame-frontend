03abfee8dd953abd9c85bb954f5768ab49ed3db1
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-28 12:27:47 +0100

    Complete funcionality to see and delete coins

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,15 +1,68 @@
 <template>
   <div>
     <app-header></app-header>
-    <h1>Admin page</h1>
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-9">
+          <h3 class="text-center">List of coins</h3>
+          <p v-if="loading" class="text-center mt-5">
+            Loading cryptocoins data...
+          </p>
+          <div v-else class="coins-cont">
+            <p
+              v-if="coins.error"
+              class="bg-danger rounded text-light mt-4 text-center"
+            >
+              {{ coins.error }}
+            </p>
+            <coins-admin
+              v-else
+              v-bind:coins="coins.data"
+              v-on:updated="refreshTable()"
+            ></coins-admin>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import AppHeader from "./partials/Header.vue";
+import CoinsAdmin from "./partials/CoinsAdmin.vue";
+import { coinsTableUrl } from "../config/config.js";
+
 export default {
+  data() {
+    return {
+      coins: null,
+      loading: true,
+      error: null,
+      success: null,
+    };
+  },
   components: {
     AppHeader,
+    CoinsAdmin,
+  },
+  methods: {
+    requestData() {
+      this.$http.get(coinsTableUrl).then((response) => {
+        this.coins = response;
+        this.loading = false;
+      });
+    },
+    refreshTable() {
+      this.requestData();
+      $("coins-admin").remove();
+      let el = $(
+        '<coins-admin v-else v-bind:coins="coins.data" v-on:updated="refreshTable()"></coins-admin>'
+      );
+      $(".coins-cont").append(el);
+    },
+  },
+  mounted() {
+    this.requestData();
   },
 };
 </script>
